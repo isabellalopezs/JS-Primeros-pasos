dcdc7d2422dfb4a84244108f419e6155adbf1c30
Isabella <lopezisabella102@gmail.com>
2022-12-12 17:21:19 -0500

    first commit

---
+++ BuenasPracticas.js
@@ -0,0 +1,18 @@
+let nombreEmpleado = "Pirula";
+let NombreEmpleado = "Hera";
+
+console.log(nombreEmpleado);
+console.log(NombreEmpleado);
+
+
+let valorBoleto = 1650;
+const porcentajeTasaEmbarque = 60/100;
+var gestionAgencia = 120.55;
+console.log(valorBoleto);
+
+let boletoActivo = true;
+
+//Operaciones Aritméticas
+
+let totalBoletos = valorBoleto + (valorBoleto*porcentajeTasaEmbarque) + gestionAgencia;
+console.log(totalBoletos);
---
+++ Listas.js
@@ -0,0 +1,16 @@
+
+//Definición de una lista con tipo de datos alfanumérico
+const ciudadesDisponibles = new Array("Bogotá", "Lima","Santiago");
+console.log(ciudadesDisponibles);
+
+//Definición de lista de forma abreviada
+const paisesDisponibles = ["Colombia","Perú" , "Brasil", "Chile"];
+console.log(paisesDisponibles);
+
+{
+    paisesDisponibles.push("Uruguay");
+    ciudadesDisponibles.push("Montevideo");
+
+    console.log(paisesDisponibles);
+    console.log(ciudadesDisponibles);
+}
